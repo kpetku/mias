2a83162e3722a4e72db95e5d084e1efd436b9ef1
Keith Petkus <keith@keithp.net>
2022-05-02 18:28:21 -0400

    Update images, templates, and configs to prepare for host networking

--- vmagent/wrapper.sh
+++ vmagent/wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cmd1="/project/node_exporter --web.listen-address=0.0.0.0:9100 --path.procfs=/host/proc --path.rootfs=/rootfs --path.sysfs=/host/sys --collector.filesystem.mount-points-exclude=^/(sys|proc|dev|host|etc)($$|/)"
-cmd2="/project/vmagent-prod -promscrape.config=./prometheus.yml -remoteWrite.tmpDataPath=/opt/vmagent/storage -remoteWrite.url=http://vmgateway:8428/api/v1/write"
+cmd1="/project/node_exporter --web.listen-address=127.0.0.1:9100 --path.procfs=/host/proc --path.rootfs=/rootfs --path.sysfs=/host/sys --collector.filesystem.mount-points-exclude=^/(sys|proc|dev|host|etc)($$|/)"
+cmd2="/project/vmagent-prod -promscrape.config=./prometheus.yml -remoteWrite.tmpDataPath=/opt/vmagent/storage -remoteWrite.url=http://${MIAS_DOMAIN}:8428/api/v1/write"
 
 $cmd1 &
 # Start the second process
