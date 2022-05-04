46699f7f6f38c6ffde660e099cd74c0e05b093b4
Keith Petkus <keith@keithp.net>
2022-05-04 10:08:59 -0400

    Wait 60 seconds to prevent a possible startup file lock data race in vmagent

--- vmagent/wrapper.sh
+++ vmagent/wrapper.sh
@@ -4,9 +4,13 @@ cmd1="/project/node_exporter --web.listen-address=127.0.0.1:9100 --path.procfs=/
 cmd2="/project/vmagent-prod -promscrape.config=./prometheus.yml -remoteWrite.tmpDataPath=/opt/vmagent/storage -remoteWrite.url=http://${MIAS_DOMAIN}:8428/api/v1/write"
 
 $cmd1 &
-# Start the second process
 
+# Wait 60 seconds to prevent a possible startup file lock data race in vmagent
+sleep 60
+
+# Start the second process
 $cmd2 &
+
 # Wait for any process to exit
 wait -n
 
